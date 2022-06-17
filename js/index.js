834f8ab9a0df23461338ce71ecee3b61e724c9fb
beqa kereselidze <beqa.kereselidze@gmail.com>
2022-06-17 10:35:52 +0400

    დავამატე ლიები

--- js/index.js
+++ js/index.js
@@ -2,10 +2,15 @@ import MyArr, {array3} from "./helper.js";
 let newArr = new MyArr('ana','nino','gela','bacho')
 console.log(newArr.arrPush('bla'))
 
-document.getElementById('dachera').addEventListener('click',()=>{
-    const resultText = array3.join('');
-    const para = document.createElement('li');
-    const textNode = document.createTextNode(resultText);
-    para.appendChild(textNode);
-    document.querySelector("#liebi").appendChild(para)
-})
+
+
+function getLies(){
+    setTimeout(() => {
+        let output = '';
+        array3.forEach(element => {
+            output += `<li>this is ${element}</li>`
+        });
+        document.body.innerHTML = output
+    }, 2000);
+}
+getLies()
